224f5ddac0997df5f527f179b053bd6b01be40dc
Daneryl <daneryl@gmail.com>
2020-10-02 16:14:28 +0200

    spreadsheet reducer with basic + operator support

--- src/specs/spreadsheet.spec.ts
+++ src/specs/spreadsheet.spec.ts
@@ -1,26 +1,25 @@
-import { Spreadsheet } from '../Spreadsheet';
+import { spreadsheetReducer, Spreadsheet } from '../spreadsheetReducer';
 
-describe('spreadsheet', () => {
-  beforeEach(() => {
-    Spreadsheet.clear();
+describe('spreadsheet reducer', () => {
+  it('should save value passed for the cell, without changing other cells', () => {
+    const prevState = { a34: { value: '66' } };
+    const action = { cell: 'a1', value: '5' };
+    const state = spreadsheetReducer(prevState, action);
+
+    expect(state).toEqual({ a1: { value: '5' }, a34: { value: '66' } });
   });
 
-  it('should save value passed for the cell', () => {
-    Spreadsheet.updateCell('value', '1', 'a');
-    Spreadsheet.updateCell('value2', '5', 'c');
+  it('should be able to use + operations referencing other cells', () => {
+    let prevState: Spreadsheet = { a2: { value: '66' }, b3: { value: '5' } };
+    let action = { cell: 'a1', value: '=a2+b3' };
+    let state = spreadsheetReducer(prevState, action);
 
-    let value :string = Spreadsheet.getCellValue('1', 'a');
-    expect(value).toBe('value');
+    expect(state.a1.value).toEqual(71);
 
-    value = Spreadsheet.getCellValue('5', 'c');
-    expect(value).toBe('value2');
-  });
+    prevState = { c2: { value: '2' }, b3: { value: '5' } };
+    action = { cell: 'a1', value: '=c2+b3' };
+    state = spreadsheetReducer(prevState, action);
 
-  it('should return empty string on unset cells', () => {
-    let value :string = Spreadsheet.getCellValue('1', 'a');
-    expect(value).toBe('');
-
-    value = Spreadsheet.getCellValue('5', 'c');
-    expect(value).toBe('');
+    expect(state.a1.value).toEqual(7);
   });
 });
